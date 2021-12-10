30460f7602c5dde36bf4363e08f59cf28a6a802d
yanjinghui <yanjinghui>
2021-12-10 14:42:29 +0800

    add jlog2

---
+++ jlog2/go.mod
@@ -0,0 +1,5 @@
+module gitee.com/hhvy/golib/jlog2
+
+go 1.16
+
+require gitee.com/hhvy/golib/jlog v0.0.0-20211210054824-e13bdbaed8db
---
+++ jlog2/log.go
@@ -0,0 +1,37 @@
+package jlog2
+
+import "gitee.com/hhvy/golib/jlog"
+
+var LogRoot string = "logs"
+var LogName string = "day"
+var LogTail string = "log"
+var _logUtil *jlog.LogUtils = nil
+
+func _initLog() {
+	if _logUtil == nil {
+		_logUtil = jlog.BuildDayLogUtils(LogRoot, LogName, LogTail)
+	}
+}
+
+func LogDebug(text string) {
+	_initLog()
+
+	_logUtil.Debug(text)
+}
+
+func LogInfo(text string) {
+	_initLog()
+
+	_logUtil.Info(text)
+}
+func LogError(text string) {
+	_initLog()
+
+	_logUtil.Error(text)
+}
+
+func LogWarn(text string) {
+	_initLog()
+
+	_logUtil.Warn(text)
+}
